9e49dde1dbb78bb98fbace34f1a26fdc59e73169
jrge <jorgemoymoya@gmail.com>
2026-03-25 22:26:39 -0300

    Add protection ticket_routes

--- app/routes/ticket_routes.py
+++ app/routes/ticket_routes.py
@@ -1,10 +1,12 @@
 from fastapi import APIRouter, FastAPI, HTTPException
+from app.services.auth_service import get_current_active_user
 from app.services.ticket_services import ticket_service
 from pydantic import BaseModel
 from app.schemas.schemas import TicketOpenIn, TicketOpenOut, TicketPayIn
 from app.schemas.schemas import ReceiptPayOut
 from app.exceptions import TicketPaidError
-from typing import List
+from typing import Annotated, List
+from fastapi import Depends
 
 router = APIRouter(
     prefix="/tickets",
@@ -13,14 +15,18 @@ router = APIRouter(
 
 
 @router.get("/open", response_model=List[TicketOpenOut])
-def get_ticket():
+def get_ticket(token: Annotated[str, Depends(get_current_active_user)]):
     result = ticket_service.get_open_tickets()
     return result
 
 
 @router.post("", response_model=TicketOpenOut)
-def open_ticket(item: TicketOpenIn):
-    print(item.patente)
+def open_ticket(
+    item: TicketOpenIn, token: Annotated[str, Depends(get_current_active_user)]
+):
+    print(
+        item.patente,
+    )
     try:
         ticket_info = ticket_service.open_ticket(patente=item.patente)
         return ticket_info
@@ -30,13 +36,15 @@ def open_ticket(item: TicketOpenIn):
 
 
 @router.get("/{id}", response_model=TicketOpenOut)
-def get(id: int):
+def get(id: int, token: Annotated[str, Depends(get_current_active_user)]):
     result = ticket_service.get_by_id(id)
     return result
 
 
 @router.post("/pay", response_model=ReceiptPayOut)
-def pay_ticket(item: TicketPayIn):
+def pay_ticket(
+    item: TicketPayIn, token: Annotated[str, Depends(get_current_active_user)]
+):
     service = ticket_service
     try:
         boleta = service.pay_ticket(item.id)
