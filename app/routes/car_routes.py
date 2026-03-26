1cb84c41628283790c6d671448fae3830e635dfd
jrge <jorgemoymoya@gmail.com>
2026-03-26 14:36:09 -0300

    Add protection to car routes

--- app/routes/car_routes.py
+++ app/routes/car_routes.py
@@ -1,7 +1,9 @@
 from fastapi import APIRouter, HTTPException
 from app.schemas.schemas import Car, CarCreate
 from app.services.car_services import car_service
-from typing import List
+from typing import List, Annotated
+from app.services.auth_service import get_current_active_user
+from fastapi import Depends
 
 router = APIRouter(
     prefix="/cars",
@@ -10,18 +12,18 @@ router = APIRouter(
 
 
 @router.get("", response_model=List[Car])
-def get_cars():
+def get_cars(token: Annotated[str, Depends(get_current_active_user)]):
     result = car_service.get_cars()
     return result
 
 
 @router.delete("")
-def delete_car(id: int):
+def delete_car(id: int, token: Annotated[str, Depends(get_current_active_user)]):
     pass
 
 
 @router.post("", response_model=Car)
-def create_car(car: CarCreate):
+def create_car(car: CarCreate, token: Annotated[str, Depends(get_current_active_user)]):
     try:
         result = car_service.create_car(car.patente)
         return result
@@ -31,7 +33,7 @@ def create_car(car: CarCreate):
 
 
 @router.post("/{id}", response_model=Car)
-def get_car_by_id(id: int):
+def get_car_by_id(id: int, token: Annotated[str, Depends(get_current_active_user)]):
     try:
         result = car_service.get_by_id(id)
         print(result, "en controller")
